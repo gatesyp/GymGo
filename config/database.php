a7b9042a417ded66df6d625da69da8cd6cb986ee
Matt Gates <info@mgates.me>
2016-02-20 14:03:03 -0500

    wtf steve

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'GymGo'),
+            'database'  => env('DB_DATABASE', ''),
             'username'  => env('DB_USERNAME', 'root'),
             'password'  => env('DB_PASSWORD', 'root'),
             'charset'   => 'utf8',
