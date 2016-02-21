565dfb576d0201cf63f4c55f0c77d75528be1fd7
Matt Gates <info@mgates.me>
2016-02-21 11:00:34 -0500

    act

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -2,94 +2,77 @@
 
 @section('content')
 
-    <?php
-        $k = json_decode(file_get_contents('http://api.reimaginebanking.com/accounts/56c66be6a73e492741507c8f/transfers?key=fab8d98dc85bc29afbe6b9915f27a0e7'));
+    <form class="form-horizontal">
+        <fieldset>
 
-    ?>
+            <!-- Form Name -->
+            <legend>Add CapitalOne Bank</legend>
 
-    <div class="row">
-        <div class="col-md-6">
-            <h2>Revenue for Trainers</h2>
-            <canvas id="lineChart"></canvas>
-        </div>
-        <div class="col-md-6">
-            <h2>Client Activity</h2>
-            <canvas id="barChart"></canvas>
-        </div>
-    </div>
-    <h2 class="text-center">Exercises with most issues</h2>
-    <canvas id="radChart"></canvas>
+            <!-- Text input-->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="textinput">First name</label>
+                <div class="col-md-4">
+                    <input id="textinput" name="textinput" type="text" placeholder="John" class="form-control input-md">
 
-@endsection
+                </div>
+            </div>
 
-@section('customjs')
-    <script>
+            <!-- Text input-->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="textinput">Last Name</label>
+                <div class="col-md-4">
+                    <input id="textinput" name="textinput" type="text" placeholder="Smith" class="form-control input-md">
 
-        var option = {
-            responsive: true,
-        };
+                </div>
+            </div>
 
-        var data = {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
-            datasets: [
-                {
-                    label: "My First dataset",
-                    fillColor: "rgba(220,220,220,0.2)",
-                    strokeColor: "rgba(220,220,220,1)",
-                    pointColor: "rgba(220,220,220,1)",
-                    pointStrokeColor: "#fff",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(220,220,220,1)",
-                    data: [65, 59, 80, 81, 56, 55, 40]
-                },
-                {
-                    label: "My Second dataset",
-                    fillColor: "rgba(151,187,205,0.2)",
-                    strokeColor: "rgba(151,187,205,1)",
-                    pointColor: "rgba(151,187,205,1)",
-                    pointStrokeColor: "#fff",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(151,187,205,1)",
-                    data: [28, 48, 40, 19, 86, 27, 90]
-                }
-            ]
-        };
+            <!-- Text input-->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="textinput">Address</label>
+                <div class="col-md-4">
+                    <input id="textinput" name="textinput" type="text" placeholder="3910 Kent Road" class="form-control input-md">
 
-        // Get the context of the canvas element we want to select
-        var ctx = document.getElementById("lineChart").getContext('2d');
-        var myLineChart = new Chart(ctx).Line(data, option); //'Line' defines type of the chart.
+                </div>
+            </div>
 
-        var ctx = document.getElementById("barChart").getContext('2d');
-        var myBarChart = new Chart(ctx).Bar(data, option);
+            <!-- Text input-->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="textinput">City</label>
+                <div class="col-md-4">
+                    <input id="textinput" name="textinput" type="text" placeholder="Stow" class="form-control input-md">
 
-        var data = {
-            labels: ["Leg Press", "Chest Press", "Lateral Pulldown", "Pullup", "Bicep Curl", "Dumbbell Fly", "Shrug"],
-            datasets: [
-                {
-                    label: "My First dataset",
-                    fillColor: "rgba(220,220,220,0.2)",
-                    strokeColor: "rgba(220,220,220,1)",
-                    pointColor: "rgba(220,220,220,1)",
-                    pointStrokeColor: "#fff",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(220,220,220,1)",
-                    data: [65, 59, 80, 81, 56, 55, 40]
-                },
-                {
-                    label: "My Second dataset",
-                    fillColor: "rgba(151,187,205,0.2)",
-                    strokeColor: "rgba(151,187,205,1)",
-                    pointColor: "rgba(151,187,205,1)",
-                    pointStrokeColor: "#fff",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(151,187,205,1)",
-                    data: [28, 48, 40, 19, 86, 27, 90]
-                }
-            ]
-        };
+                </div>
+            </div>
 
-        var ctx = document.getElementById("radChart").getContext('2d');
-        var myRadarChart = new Chart(ctx).Radar(data, option);
+            <!-- Text input-->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="textinput">State</label>
+                <div class="col-md-4">
+                    <input id="textinput" name="textinput" type="text" placeholder="Ohio" class="form-control input-md">
 
-    </script>
+                </div>
+            </div>
+
+            <!-- Text input-->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="textinput">Bank Account Number</label>
+                <div class="col-md-4">
+                    <input id="textinput" name="textinput" type="text" placeholder="290190875" class="form-control input-md">
+
+                </div>
+            </div>
+
+            <!-- Text input-->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="textinput">Zip</label>
+                <div class="col-md-4">
+                    <input id="textinput" name="textinput" type="text" placeholder="44224" class="form-control input-md">
+
+                </div>
+            </div>
+
+            <button type="submit" class="btn btn-block btn-warning waves-effect waves-light">Add my bank!</button>
+        </fieldset>
+    </form>
+
 @endsection
