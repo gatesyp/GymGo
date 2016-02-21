b887aa9b9de8811e22558fbf5208db0bc8de2d0a
Matt Gates <info@mgates.me>
2016-02-20 21:12:01 -0500

    latest

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,73 +1,10 @@
 @extends('master')
 
 @section('content')
-    <?php $images = [
-            'http://image.shutterstock.com/display_pic_with_logo/90390/270379067/stock-photo-young-people-training-in-the-gym-270379067.jpg',
-            'http://thumb1.shutterstock.com/display_pic_with_logo/304216/304216,1326758103,10/stock-photo-group-with-dumbbell-weight-training-equipment-on-sport-gym-92905789.jpg',
-            'http://thumb1.shutterstock.com/display_pic_with_logo/84610/123602794/stock-photo-young-people-group-of-women-and-men-doing-sport-biking-in-the-gym-for-fitness-123602794.jpg',
-            'http://thumb1.shutterstock.com/display_pic_with_logo/540181/167065400/stock-photo-pretty-girl-working-out-in-a-treadmill-at-the-gym-and-smiling-167065400.jpg',
-            'http://thumb9.shutterstock.com/display_pic_with_logo/76219/298454666/stock-photo-front-view-of-a-muscular-couple-doing-planking-exercises-298454666.jpg',
-            'http://thumb7.shutterstock.com/display_pic_with_logo/163108/314302445/stock-photo-young-woman-runner-resting-after-workout-session-on-sunny-morning-female-fitness-model-sitting-on-314302445.jpg',
-            'http://thumb101.shutterstock.com/display_pic_with_logo/345733/337042967/stock-photo-male-runner-doing-stretching-exercise-preparing-for-morning-workout-in-the-park-337042967.jpg',
-            'http://thumb1.shutterstock.com/display_pic_with_logo/1032538/306238778/stock-photo-sportsman-doing-crunches-in-the-gym-306238778.jpg',
-            'http://thumb7.shutterstock.com/display_pic_with_logo/113008/169282331/stock-photo-personal-trainer-helping-woman-working-with-heavy-dumbbells-169282331.jpg',
-            'http://thumb101.shutterstock.com/display_pic_with_logo/950635/269886053/stock-photo-sportsmen-fit-male-trainer-man-and-woman-doing-clapping-push-ups-explosive-strength-training-269886053.jpg',
-            'http://thumb1.shutterstock.com/display_pic_with_logo/64260/337161530/stock-photo-sport-bodybuilding-lifestyle-and-people-concept-young-man-and-woman-with-barbell-flexing-337161530.jpg',
-    ];
+    <div class="divider-new">GymGo Clients</div>
 
-    $names = ['Reid',
-            'Bernardina',
-            'Lin',
-            'Eloy',
-            'Loren',
-            'Collin',
-            'Cathleen',
-            'Reba',
-            'Elke',
-            'Lucile',
-            'Mariko',
-            'Kimberlie',
-            'Erich',
-            'Elois',
-            'Gregorio',
-            'Natosha',
-            'Denese',
-            'Annett',
-            'Tania',
-            'Evelyn',
-            'Latrice',
-            'Renate',
-            'Rosalina',
-            'Lesley',
-            'Claudia',
-            'Imelda',
-            'Sherrell',
-            'Kareem',
-            'Obdulia',
-            'Bronwyn',
-            'Berneice',
-            'Aleisha',
-            'Allyn',
-            'Cedrick',
-            'Aletha',
-            'Ben',
-            'Rowena',
-            'Tonia',
-            'Hope',
-            'Rosemary',
-            'Mariano',
-            'Malvina',
-            'Kenya',
-            'Minh',
-            'Kelsi',
-            'Latoyia',
-            'Coralie',
-            'Nigel',
-            'Charity',
-            'Dolly',];
-    ?>
-
-    <div class="divider-new">Clients Requesting Assistance</div>
+    <button class="waves-effect waves-light btn btn-default filter" data-filter="all">All</button>
+    <button class="waves-effect waves-light btn btn-default filter" data-filter=".category-1">Requesting help</button>
 
     <!--Main row-->
     <div class="row client-cards">
@@ -116,40 +53,48 @@
 @endsection
 
 @section('customjs')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/mixitup/2.1.11/jquery.mixitup.min.js"></script>
     <script>
-        String.prototype.capitalize = function() {
+        function getRandomArbitrary(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+        String.prototype.capitalize = function () {
             return this.charAt(0).toUpperCase() + this.slice(1);
         }
+
         $(document).ready(function () {
-            $.get("/issues/", function (data) {
+            $.get("/users/", function (data) {
+
+                var supports = 0;
                 $.each(data, function (k, v) {
-                    $('.client-cards').append('        <div class="col-lg-3 col-md-6">\
+                    var randSupport = false;
+                    if (getRandomArbitrary(0, 1) == 1 && supports < 3) {
+                        supports++;
+                        randSupport = true;
+                    }
+                    $('.client-cards').append('<div class="col-lg-3 col-md-6 mix ' + (randSupport ? 'category-1' : '') + '" data-my-order="' + (k + 1) + '">\
                             <!--Stylish Card Light-->\
                     <div class="card stylish-card hoverable wow fadeInUp" data-wow-delay="0.4s">\
                             <div class="view overlay hm-white-slight z-depth-1">\
-                            <img src="{{ $images[array_rand($images, 1)] }}" class="img-responsive" alt="">\
+                            <img src="http://api.randomuser.me/portraits/' + (getRandomArbitrary(0, 1) == 0 ? 'women' : 'men') + '/' + k + '.jpg" class="img-responsive" alt="">\
                             <div class="mask waves-effect activator"></div>\
                             </div>\
 \
                             <!--Content-->\
                             <div class="card-content">\
-                            <h4 class="name">' + v.user_name.capitalize() + '</h4>\
+                            <h4 class="name">' + v.user.capitalize() + '</h4>\
                             <dl class="dl-horizontal">\
                             <p class="status">Currently doing <b>' + v.exercise_name + '</b> exercises.</p>\
                     <br>\
                     <dt>Weight</dt>\
-                    <dd>{{ rand(120, 210) }} lb</dd>\
+                    <dd>' + getRandomArbitrary(120, 210) + ' lb</dd>\
                     <dt>Height</dt>\
-                    <dd>{{ rand(5, 6) }}\'{{ rand(1, 11) }}"</dd>\
+                    <dd>' + getRandomArbitrary(5, 6) + "'" + getRandomArbitrary(1, 11) + '</dd>\
                     <dt>BMI</dt>\
-                    <dd>{{ rand(11, 30) }}</dd>\
+                    <dd>' + getRandomArbitrary(11, 30) + '</dd>\
                     <dt>Personal Trainer</dt>\
                     <dd>\
-                    @if(rand(0, 1) == 1)
-                                    0:{{ sprintf("%02d", rand(0, 59)) }}:{{ sprintf("%02d", rand(0, 59)) }} left\
-                    @else
-                                    Not purchased!\
-                            @endif
+                    ' + ((getRandomArbitrary(0, 1) == 1) ? ('0:' + getRandomArbitrary(1, 59) + ':' + getRandomArbitrary(1, 59) + ' left') : 'No purchased membership') + '\
                                     </dd>\
                                     </dl>\
                                     <div class="card-footer text-right">\
@@ -175,6 +120,8 @@
                                             <!--/.Stylish Card Light-->\
                                             </div>');
                 });
+
+                $('.client-cards').mixItUp();
 
                 $('.respond').on('click', function () {
                     $('button[href="#modal1"]').click();
@@ -204,5 +151,8 @@
 
 @section('customcss')
     <style>
+        .client-cards .mix {
+            display: none;
+        }
     </style>
 @endsection
