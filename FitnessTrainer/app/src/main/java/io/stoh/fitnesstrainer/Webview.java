f2da267140a1dfa6b0ac1f5d3259bf3d990b26d1
agates4 <agate97@gmail.com>
2016-02-21 10:00:34 -0500

    finsh

--- FitnessTrainer/app/src/main/java/io/stoh/fitnesstrainer/Webview.java
+++ FitnessTrainer/app/src/main/java/io/stoh/fitnesstrainer/Webview.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  * Created by GeczySSD on 2/21/2016.
@@ -13,13 +14,13 @@ public class Webview extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.webview);
-        Uri uri = Uri.parse("https://stoh.io/gym/GymGo/public/");
-        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        startActivity(intent);
+
         WebView webview = new WebView(this);
-        setContentView(webview);
-        // Simplest usage: note that an exception will NOT be thrown
-        // if there is an error loading this page (see below).
+        webview =(WebView) findViewById(R.id.webView);
+        webview.setWebViewClient(new WebViewClient());
+        webview .getSettings().setJavaScriptEnabled(true);
+        webview .getSettings().setDomStorageEnabled(true);
         webview.loadUrl("https://stoh.io/gym/GymGo/public/");
+
     }
 }
